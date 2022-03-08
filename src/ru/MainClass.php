fd8ac36d3f9d1f1b5812b404daabcd5e8a2b45ac
JRoniel <johnroniel8264@gmail.com>
2022-03-08 20:24:22 -0300

    MASTER

--- src/ru/MainClass.php
+++ src/ru/MainClass.php
@@ -14,52 +14,52 @@ use ru\{
 };
 use PocketMine\{
 	world\WorldManager,
-    plugin\PluginBase,
-    Server
+	plugin\PluginBase,
+	Server
 };
 use function array_diff;
 use function scandir;
 
 class MainClass extends PluginBase {
 
-    public $globalMute = false;
+	public $globalMute = false;
 	public $flyAllowed = false;
 
 	private WorldManager $worldmanager;
 
-    private static $instance;
+	private static $instance;
 
 	const PREFIX = '[RU]';
 	const VERSION = '0.2-alpha';
-    const LOBBY = 'lobby';
+	const LOBBY = 'lobby';
 	const RPG_WORLD = 'rpg';
 
 	public function getWorldManager() : WorldManager{
 		return $this->worldManager;
 	}
 
-    public static function getInstance(): MainClass
+	public static function getInstance(): MainClass
 	{
 		return self::$instance;
 	}
-    
-    public function onEnable(): void 
-    {
+
+	public function onEnable(): void 
+	{
 		self::$instance = $this; 
 		$this->disableCommands();
 		$this->setCommands();
 		$this->getServer()->getPluginManager()->registerEvents(new PlayerEvents($this), $this);
 		$this->getScheduler()->scheduleDelayedRepeatingTask(new BroadcastTask($this), 200, 11000);
 		foreach (array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $worldName) {
-            $this->getWorldManager()->loadWorld($worldName);
-        }
+		$this->getWorldManager()->loadWorld($worldName);
+	}
 		foreach($this->getServer()->getLevels() as $levels){
 			foreach($levels->getEntities() as $entity){
 				$entity->close();
 			}
 		}
-        $this->getLogger()->info('RU >> Núcleo funcional e assicróno, RankUp_Class '.self::VERSION);
-    }
+		$this->getLogger()->info('RU >> Núcleo funcional e assicróno, RankUp_Class '.self::VERSION);
+	}
 
 
 	
@@ -70,8 +70,8 @@ class MainClass extends PluginBase {
 		$map->register("help", new helpCommand($this));
 	}
 
-    public function disableCommands()
-    {
+	public function disableCommands()
+	{
 		$map = $this->getServer()->getCommandMap();
 		$map->unregister($map->getCommand("kill"));
 		$map->unregister($map->getCommand("me"));
